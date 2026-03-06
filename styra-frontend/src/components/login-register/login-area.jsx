654b0ae077d6374771bf3e96b92dcd7860117bc7
rohit-gotgit <viciousdotro@gmail.com>
2026-03-06 19:46:58 +0530

    updated some values

--- styra-frontend/src/components/login-register/login-area.jsx
+++ styra-frontend/src/components/login-register/login-area.jsx
@@ -16,7 +16,7 @@ const LoginArea = () => {
             <div className="col-xl-6 col-lg-8">
               <div className="tp-login-wrapper">
                 <div className="tp-login-top text-center mb-30">
-                  <h3 className="tp-login-title">Login to Shofy.</h3>
+                  <h3 className="tp-login-title">Login to Styra</h3>
                   <p>
                     Don’t have an account?{" "}
                     <span>
